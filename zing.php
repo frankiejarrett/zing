474e6bf5707f92d50fb0d0784a2ddbdd8954d95b
Frankie Jarrett <fjarrett@gmail.com>
2016-03-29 22:18:20 -0500

    Update zing.php

--- zing.php
+++ zing.php
@@ -5,8 +5,11 @@
  * Version: 0.1.0
  * Author: Frankie Jarrett
  * Author URI: http://frankiejarrett.com
- * License: GPLv2+
  * Text Domain: zing
+ * 
+ * Copyright: © 2015 Frankie Jarrett.
+ * License: GNU General Public License v2.0
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /**
